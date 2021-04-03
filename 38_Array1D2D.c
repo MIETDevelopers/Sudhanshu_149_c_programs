9539d0b015b7b0bca1b66aef0912a2f3e69222df
sudhanshu raina <2020a1r149@gmail.com>
2021-04-03 20:21:43 +0530

    Pointer variable Intialization with the adress of another variable

--- 38_Array1D2D.c
+++ 38_Array1D2D.c
@@ -1,16 +1,21 @@
-#include <stdio.h> //Pre Process directive to include standard input output function header file
-void disp( char ch)
-{
-   printf("%c ", ch);
-}
-int main() //main function body starting
-{
-   char arr[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'}; // Array Declaration
-   for (int x=0; x<10; x++)
-   {
-       /* I’m passing each element one by one using subscript*/
-       disp (arr[x]);
-   }
+#include <stdio.h>
+int main() {
+  int r, c, a[100][100], b[100][100], sum[100][100], i, j;
+  printf("Enter the number of rows (between 1 and 100): ");
+  scanf("%d", &r);
+  printf("Enter the number of columns (between 1 and 100): ");
+  scanf("%d", &c);
 
-   return 0;
-}
+  printf("\nEnter elements of 1st matrix:\n");
+  for (i = 0; i < r; ++i)
+    for (j = 0; j < c; ++j) {
+      printf("Enter element a%d%d: ", i + 1, j + 1);
+      scanf("%d", &a[i][j]);
+    }
+
+  printf("Enter elements of 2nd matrix:\n");
+  for (i = 0; i < r; ++i)
+    for (j = 0; j < c; ++j) {
+      printf("Enter element b2%d%d: ", i + 1, j + 1);
+      scanf("%d", &b[i][j]);
+    }
